de39a76d4f4af1c68a92dff66d23d0bbab98c40c
vikas <vikas.vm788@gmail.com>
2020-08-13 13:23:40 +0530

    commit

--- src/components/movie-details.js
+++ src/components/movie-details.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faStar} from "@fortawesome/free-solid-svg-icons";
 
 const MovieDetails = (props) =>{
     const mov = props.movie;
+    const [highLighted, setHighLighted] = useState(-1);
+    const highLightRate = high => evt => {
+        setHighLighted(high)
+    };
+    const rateClicked = rate => evt =>{
+        fetch(`http://127.0.0.1:8000/api/movie/${mov.id}/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Token e9f700dfc42250606962cf9c1fc26cbcf02eb710'
+      },
+            body: JSON.stringify({stars:rate+1})
+    })
+        .then(resp => resp.json())
+        .then(resp => console.log(resp))
+        .catch(error => console.log(error))
+    };
     return(
-        <div>
+        <React.Fragment>
             {mov ? (
                 <div>
                     <h1>{props.movie && props.movie.title}</h1>
@@ -16,9 +33,19 @@ const MovieDetails = (props) =>{
                     <FontAwesomeIcon icon={faStar} className={mov.avg_rating > 2 ? 'orange':''}/>
                     <FontAwesomeIcon icon={faStar} className={mov.avg_rating > 3 ? 'orange':''}/>
                     <FontAwesomeIcon icon={faStar} className={mov.avg_rating > 4 ? 'orange':''}/>
+                    <div className={'rating-box'}>
+                        <h1>Rate it</h1>
+                        {
+                            [...Array(5)].map((e, i) => {
+                                return <FontAwesomeIcon key={i} icon={faStar} className={highLighted > i - 1 ? 'purple':''}
+                                                        onMouseEnter={highLightRate(i)} onMouseLeave={highLightRate(-1)}
+                                onClick={rateClicked(i)}/>
+                            })
+                        }
+                    </div>
                 </div>
             ) : null}
-        </div>
+        </React.Fragment>
 
 )
 };
